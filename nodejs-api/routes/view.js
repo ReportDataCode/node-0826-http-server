f031a9f1d7f09195f5a53acd6e02cb2e5d2c6986
Prince Anire <andrewcustomerhelper@gmail.com>
2021-09-02 10:33:09 +0800

    added json object

--- nodejs-api/routes/view.js
+++ nodejs-api/routes/view.js
@@ -2,25 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/view', (req, res) => {
-    // res.send('view list and hello andrew!');
-    res.json({
-        "firstName": "Juan",
-        "lastName": "Dela Cruz",
-        "age": 20
+    res.send(req.query)
+    console.log(req.body);
+})
 
-    });
-});
+// router.get('/view', (req, res) => {
+//     // res.send('view list and hello andrew!');
+//     res.json({
+//         "firstName": "Juan",
+//         "lastName": "Dela Cruz",
+//         "age": 20
+
+//     });
+// });
 
 router.post('/insert', (req, res) => {
     // res.send('view list and hello andrew!');
     res.json({
-        "message": "success",
     });
+    console.log(req.body);
 });
 
 
 router.get('/', (req, res) => {
     res.send("This works andrew!");
+    
 });
 
 
